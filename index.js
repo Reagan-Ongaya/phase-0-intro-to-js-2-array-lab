526c5fceffb008da6c1e37c1bcf30123e7de966b
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-12-07 17:59:38 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-beforeEach(functiobn () {
-    cats.length = 0;
+let cats =['Milo', 'Otis', 'Garfield'];
 
-    cats.push("Milo", "Otis" "Garfield"); 
-});
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+    return cats;
+}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(Broom){
+    cats.push(Broom);
+    return cats;
+}
+
+function prependCat(Arnold){
+    cats.unshift(Arnold);
+    return cats ;
+}
+
+function removeLastCat(){
+    cats.pop('Garfield');
+    return cats;
+}
+
+function  removeFirstCat(){
+    cats.shift('Milo');
+    return cats;
+}
